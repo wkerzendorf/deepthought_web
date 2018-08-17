41133bf0ef399f08607a3bf653198f5780759a94
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-08-17 10:21:06 +0200

    adding stuff

--- deepthought_web.py
+++ deepthought_web.py
@@ -5,6 +5,7 @@ import cherrypy
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
+from scipy import sparse
 import re
 import os
 from jinja2 import Environment, FileSystemLoader
@@ -25,16 +26,47 @@ config = {
     'tools.staticdir.dir' : os.path.join(path, 'fonts')
   }
 }
-env = Environment(loader=FileSystemLoader(os.path.join(path, 'templates')))
+env = Environment(loader=FileSystemLoader('templates'))
+
+
+class SKTFIDFCompare(object):
+    
+    similarity = None
+    
+    @classmethod
+    def from_hdf(cls, fname):
+        meta = pd.read_hdf(fname, 'meta')
+        df_matrix = pd.read_hdf(fname, 'tfidf_matrix')
+        matrix = sparse.coo_matrix((df_matrix.tfidf.values, 
+                                    (df_matrix.row, df_matrix.col.values))).tocsr()
+        vocabulary = pd.read_hdf(fname, 'vocabulary')
+        return cls(matrix, vocabulary, meta)
+    
+    def __init__(self, matrix, vocabulary, meta):
+        self.matrix = matrix
+        self.vocabulary = vocabulary
+        self.meta = meta
+    
+    def get_doc_vector(self, article_id):
+        self.cur_article_id = article_id
+        paper_id = self.meta.index.get_loc(article_id)
+        doc_vector = self.matrix[paper_id]
+        self.vocabulary['cur_word_weight'] = np.squeeze(doc_vector.A)
+        return doc_vector
+
+    def compare_paper(self, article_id):
+        self.cur_article_id = article_id
+        doc_vector = self.get_doc_vector(article_id)
+        self.similarity = np.squeeze((self.matrix * doc_vector.T).A)
+        self.ranked_similarity = np.argsort(self.similarity)[::-1]
+        return self.similarity
+        #
+
 
 class DeepThought(object):
 
     def __init__(self):
-        return
-        self.all_identifiers = pickle.load(open('all_identifiers.pkl'))
-        self.X_tfidf = load_sparse_csr('x_tfidf.csr.npz')
-        self.meta = pickle.load(open('meta.pkl'))
-        self.tfidf_vect = pickle.load(open('tfidf_vect.pkl'))
+        self.dt_tfidf = SKTFIDFCompare.from_hdf('dt_201806_tfidf_skl.h5')
 
 
     @cherrypy.expose
@@ -47,26 +79,31 @@ class DeepThought(object):
     def arxiv_search(self, identifier='1207.4481'):
         identifier = identifier.strip()
         template = env.get_template('arxiv_search')
-        if identifier not in self.all_identifiers:
+        if identifier not in self.dt_tfidf.meta.index:
             return template.render(identifier=identifier, unknown_id=True)
         else:
-            test_document_id = self.all_identifiers.index(identifier)
-            test_document = self.X_tfidf[test_document_id]
-            ranked_similarity, ranked_identifiers = self._get_similar_documents(test_document)
-            data_table = self._generate_table(ranked_similarity, ranked_identifiers)
+            similarity = self.dt_tfidf.compare_paper(identifier)
+            #test_document_id = self.dt_tfidf.meta.index.getloc(identifier)
+            #test_document = self.X_tfidf[test_document_id]
+            #ranked_similarity, ranked_identifiers = self._get_similar_documents(test_document)
+            
+            data_table = self.dt_tfidf.meta.copy().iloc[self.dt_tfidf.ranked_similarity]
+            data_table['similarity'] = similarity[self.dt_tfidf.ranked_similarity]
+            data_table['identifier'] = data_table.index
+            data_table['link'] = ['https://arxiv.org/abs/{0}'.format(identifier) for identifier in data_table['identifier']]
 
-            return template.render(identifier=identifier, data_table=data_table)
+            return template.render(identifier=identifier, data_table=data_table.iloc[:50].to_dict('records'))
         #return ''.join(random.sample(string.hexdigits, int(length)))
     
 
-    @cherrypy.expose
-    def text_search(self, text='astronomy galaxy star'):
-        test_document = self.tfidf_vect.transform([text])
-        ranked_similarity, ranked_identifiers = self._get_similar_documents(test_document)
-        data_table = self._generate_table(ranked_similarity, ranked_identifiers)
+    #@cherrypy.expose
+    #def text_search(self, text='astronomy galaxy star'):
+    #    test_document = self.tfidf_vect.transform([text])
+    #    ranked_similarity, ranked_identifiers = self._get_similar_documents(test_document)
+    #    data_table = self._generate_table(ranked_similarity, ranked_identifiers)
 
-        template = env.get_template('arxiv_search')
-        return template.render(search_str=text, data_table=data_table)
+        #template = env.get_template('arxiv_search')
+        #return template.render(search_str=text, data_table=data_table)
 
 
     def _generate_table(self, ranked_similarity, ranked_identifiers):
@@ -107,22 +144,18 @@ class DeepThought(object):
         ranked_identifiers = np.array(self.all_identifiers)[similarity_argsort]
         return ranked_similarity, ranked_identifiers
 
-
-def save_sparse_csr(filename, array):
-    np.savez(filename,data = array.data ,indices=array.indices,
-             indptr =array.indptr, shape=array.shape )
-
-def load_sparse_csr(filename):
-    loader = np.load(filename)
-    return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
-                         shape = loader['shape'])
-
-dt = DeepThought()
-cherrypy.quickstart(dt, '/', config)
-
-#cherrypy.tree.mount(my_controller.Root(), script_name=´´, config=app_conf)
-#if __name__ == '__main__':
-#    print('loading...')
-
-#    print("loading done")
+if __name__ == '__main__':
+    print('loading...')
+    dt = DeepThought()
+    print("loading done")
+    cherrypy.quickstart(dt, '/', config)
     # cherrypy.quickstart(dt)
+
+#dt_app = cherrypy.tree.mount(DeepThought())
+#print(cherrypy.config, dt_app.config, cherrypy.request.app.config['deepthought'])
+
+#cherrypy.quickstart(dt, '/')
+
+#cherrypy.engine.start()
+#print(cherrypy.config, dt_app.config, cherrypy.request.app.config['deepthought'])
+#cherrypy.engine.block()
